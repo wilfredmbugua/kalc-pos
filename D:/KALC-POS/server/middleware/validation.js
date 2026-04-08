48215852638cbdb9be4d3bfc71090d7e6a48b077
wilfredmbugua <33458251+wilfredmbugua@users.noreply.github.com>
2026-04-08 16:46:55 +0300

    Create missing validateSchema middleware function

---
+++ D:/KALC-POS/server/middleware/validation.js
@@ -0,0 +1,13 @@
+// validateSchema middleware
+
+const validateSchema = (schema) => {
+    return (req, res, next) => {
+        const { error } = schema.validate(req.body);
+        if (error) {
+            return res.status(400).send({ error: error.details[0].message });
+        }
+        next();
+    };
+};
+
+module.exports = validateSchema;
